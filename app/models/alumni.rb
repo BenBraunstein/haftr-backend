0c54096f74961c170bb53485bea8d38655a1f64a
Ben Braunstein <benzbraunstein@gmail.com>
2020-04-07 11:25:40 -0400

    created some string-array combos

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,2 +1,22 @@
 class Alumni < ApplicationRecord
+
+    def clubsArr
+        return self.clubs.split(',')
+    end
+
+    def sportsTeamsArr
+        return self.sportsTeams.split(',')
+    end
+
+    def cheerleadingArr
+        return self.cheerleading.split(',')
+    end
+
+    def awardsArr
+        return self.awards.split(',')
+    end
+
+    def committeesArr
+        return self.committees.split(',')
+    end
 end
