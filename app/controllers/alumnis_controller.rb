517c2ccdb4f252f9dbabc80fa292f16ea5ba2d45
Ben Braunstein <BenZBraunstein@gmail.com>
2020-04-13 11:30:32 -0400

    created a method for posting a new alum

--- app/controllers/alumnis_controller.rb
+++ app/controllers/alumnis_controller.rb
@@ -4,5 +4,14 @@ class AlumnisController < ApplicationController
         alumnis = Alumni.all 
         render json: alumnis
     end
+    def create 
+        byebug
+        alum = Alumni.create(strong_alum_params)
+        render json: alum
+    end
+
+    def strong_alum_params
+        params.require(:alumni).permit(:hillel, :hili, :haftr, :birthday, :firstName, :middleName, :lastName, :marriedName, :motherName, :fatherName, :currentAddress, :homePhone, :cellPhone, :workPhone, :emailAddress, :clubs, :awards, :hillelDayAttended, :hillelSleepAttended, :hillelDayYears, :hillelSleepYears, :hillelDayCamper, :hillelSleepCamper, :hillelDayCounselor, :hillelSleepCounselor, :hillelDaySpecialty, :hillelSleepSpecialty, :classParent, :boardTrustee, :boardEducation, :committees, :alumniNewsletters, :commsOutreach, :classReunions, :alumniEvents, :fundraisingEvents, :databaseResearch, :alumniChoir)
+    end
 
 end
