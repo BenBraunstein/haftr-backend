b6b098f04a035d25d56194e132836bf0ae6037a6
Ben Braunstein <BenZBraunstein@gmail.com>
2020-04-20 16:21:27 -0400

    now can do both photos and children/siblings

--- app/controllers/alumnis_controller.rb
+++ app/controllers/alumnis_controller.rb
@@ -20,17 +20,30 @@ class AlumnisController < ApplicationController
 
     def create 
         alum = Alumni.create(strong_alum_params)
-        siblings = params[:siblings]
-        children = params[:children]
-        siblings.each {|sibling| create_sibling(sibling, alum)}
-        children.each {|child| create_child(child, alum)}
         render json: alum
     end
-
+    
     def update 
         alum = Alumni.find(params[:id])
         alum.update(strong_alum_params)
         render json: alum
+    end
+    
+    def add_sibs_n_kids
+        alum = Alumni.find(params[:id])
+        siblings = params[:siblings]
+        children = params[:children]
+        siblings.each {|sibling| create_sibling(sibling, alum)}
+        children.each {|child| create_child(child, alum)}
+        if alum.photo.attached?
+            render json: {
+                alum: alum, siblings: alum.siblings, children: alum.children, photo: url_for(alum.photo)
+             }
+         else
+            render json: {
+                 alum: alum, siblings: alum.siblings, children: alum.children
+             }
+        end
     end
 
     def destroy 
@@ -40,7 +53,7 @@ class AlumnisController < ApplicationController
     end
 
     def strong_alum_params
-        params.require(:alumni).permit(:hillel, :hili, :haftr, :birthday, :firstName, :middleName, :lastName, :marriedName, :motherName, :fatherName, :currentAddress, :homePhone, :cellPhone, :workPhone, :emailAddress, :clubs, :awards, :hillelDayAttended, :hillelSleepAttended, :hillelDayYears, :hillelSleepYears, :hillelDayCamper, :hillelSleepCamper, :hillelDayCounselor, :hillelSleepCounselor, :hillelDaySpecialty, :hillelSleepSpecialty, :hiliDayAttended, :hiliDayYears, :hiliDayCamper, :hiliDayCounselor, :hiliDaySpecialty, :hiliWhiteAttended, :hiliWhiteYears, :hiliWhiteCamper, :hiliWhiteCounselor, :hiliWhiteSpecialty, :hiliInternationalAttended, :hiliInternationalYears, :hiliInternationalYears, :hiliInternationalCamper, :hiliInternationalCounselor, :hiliInternationalSpecialty, :classParent, :boardTrustee, :boardEducation, :committees, :alumniNewsletters, :commsOutreach, :classReunions, :alumniEvents, :fundraisingEvents, :databaseResearch, :alumniChoir)
+        params.require(:alumni).permit(:hillel, :hili, :haftr, :birthday, :firstName, :middleName, :lastName, :marriedName, :motherName, :fatherName, :currentAddress, :oldAddresses, :homePhone, :cellPhone, :workPhone, :emailAddress, :clubs, :awards, :hillelDayAttended, :hillelSleepAttended, :hillelDayYears, :hillelSleepYears, :hillelDayCamper, :hillelSleepCamper, :hillelDayCounselor, :hillelSleepCounselor, :hillelDaySpecialty, :hillelSleepSpecialty, :hiliDayAttended, :hiliDayYears, :hiliDayCamper, :hiliDayCounselor, :hiliDaySpecialty, :hiliWhiteAttended, :hiliWhiteYears, :hiliWhiteCamper, :hiliWhiteCounselor, :hiliWhiteSpecialty, :hiliInternationalAttended, :hiliInternationalYears, :hiliInternationalYears, :hiliInternationalCamper, :hiliInternationalCounselor, :hiliInternationalSpecialty, :classParent, :boardTrustee, :boardEducation, :committees, :alumniNewsletters, :commsOutreach, :classReunions, :alumniEvents, :fundraisingEvents, :fundraisingNetworking, :databaseResearch, :alumniChoir, :photo)
     end
 
     def create_sibling(sibling, alum)
