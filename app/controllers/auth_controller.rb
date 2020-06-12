842776570c13f09fe7062a1c050869e286c87108
Ben Braunstein <benzbraunstein@gmail.com>
2020-06-12 08:40:12 -0400

    autho controller login and autologin functions

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,27 @@
+class AuthController < ApplicationController
+
+    def login 
+        user = User.find_by('lower(email) = ?', params[:email].downcase)
+        if user && user.authenticate(params[:password])
+            render json: {
+                user: user,
+                token: JWT.encode({userId: user.id}, ENV['JWT_SECRET'])
+            }
+        else
+            render json: { errors: "Invalid username, password combination."}
+        end
+    end
+
+    def autologin
+        token = request.headers["Authorization"].split.last
+        if token != "undefined" 
+            user_id = JWT.decode(token, ENV["JWT_SECRET"])[0]["userId"]
+            user = User.find(user_id)
+            if user 
+                render json: user
+            else
+                render json: { errors: "Invalid login token, please re-log in manually"}
+            end
+        end
+    end
+end
